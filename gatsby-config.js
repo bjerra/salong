b16ba4f596a0dd7b7171712390ed6e053bf9a67b
Bjorn <ebjarne@gmail.com>
2019-11-28 23:29:21 +0100

    initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
     siteMetadata: {title: `test`, description: '', social: { facebook: ``, instagram: ''} },
     plugins: [
       {
-        resolve: "gatsby-theme-bd-salong",
+        resolve: "@bjerra/gatsby-theme-bd-salong",
         options: {     
           user: 'Anais',
           bdPath: 'skönhetsgränden--8678',
